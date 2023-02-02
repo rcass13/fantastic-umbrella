208f037ed01a920b3650e91959608d8ae7688f51
RoseCass <rosecassidy1303@gmail.com>
2023-02-02 15:06:57 -0800

    added code to routes, created server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,108 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Catagory.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include:{
+      model: Product,
+      // be sure to include its associated Products
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+    },
+  })
+
+  .then((dbCatagoryData) => {
+    if (!dbCatagoryData){
+      res.status(404).json({message:"No catagory with this id"});
+      return;
+    }
+    res.json(dbCatagoryData);
+  })
+  .catch((err) =>{
+    console.log(err);
+      res.status(500).json(err);
+  })
+  
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Catagory.create({
+    catagory_name: req.body.catagory_name,
+  })
+  .then((dbProductData)=>res.json(dbProductData))
+  .catch((err) =>{
+    console.log(err);
+      res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Catagory.update(
+    {
+      catagory_name: req.body.catagory_name
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+
+  .then((dbCatagoryData) => {
+    if (!dbCatagoryData){
+      res.status(404).json({message:"No catagory with this id"});
+      return;
+    }
+    res.json(dbCatagoryData);
+  })
+  .catch((err) =>{
+    console.log(err);
+      res.status(500).json(err);
+  })
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Catagory.destroy({
+    where:{
+      id: req.params.id,
+    }
+  })
+  .then((dbCatagoryData) => {
+    if (!dbCatagoryData){
+      res.status(404).json({message:"No catagory with this id"});
+      return;
+    }
+    res.json(dbCatagoryData);
+  })
+  .catch((err) =>{
+    console.log(err);
+      res.status(500).json(err);
+  })
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
+
 });
 
 module.exports = router;
+
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
